75b6785733a17e897908083a5ad419149eb16759
qsangdev <quangsang02018@gmail.com>
2023-04-03 20:31:16 +0700

    Test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./src/routes");
 const bodyParser = require("body-parser");
 dotenv.config()
 
